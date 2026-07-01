1e863f2ac436edb3737d5655ac9b81f0022581cf
Rosan <rosan12052006@gmail.com>
2026-07-01 15:30:26 +0530

    Update API URL for production

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 // Update this after you deploy your backend on Render.
 // Local dev: http://localhost:5000
 // Production: https://your-backend-name.onrender.com
-const API_BASE_URL = "http://localhost:5050";
+const API_BASE_URL = "https://portfolio-backend-aazd.onrender.com"
